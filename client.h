280b90fe2bc26d5e4b833510bb9314a0adb4249e
thanhtuan0310 <thanhtuan102001@gmail.com>
2023-01-27 23:02:47 +0700

    add server client

---
+++ client.h
@@ -0,0 +1,31 @@
+//* Menu
+/**
+ * Menu đăng nhập
+ * 1. Login
+ * 2. CreateAccount
+ * 3. Quit
+*/
+void LoginMenu();
+
+/**
+* 1. Play with computer
+* 2. Play with other player
+* 3. Chess puzzle
+* 4. Add friends
+* 5. View chess ranking
+* 6. View match history
+* 7. Change password
+* 8. Log out
+*/
+void UserMenu();
+
+
+/**
+ * 1. Easy
+ * 2. Normal
+ * 3. Hard
+*/
+void ChooseDifficultyComputer();
+
+
+
